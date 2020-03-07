486ef71ae5d2a95c32d0ba787bb72c8374d0a948
sagormahtab <sagormahtab@gmail.com>
2020-03-08 01:49:30 +0600

    first day code practice

--- 2.3-three-angle-of-triangle.cpp
+++ 2.3-three-angle-of-triangle.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 int main(){
-  double a,b,c,A,B,C,cosA,cosB,cosC,PI;
+  double a,b,c,A,B,C,doaCosA,doaCosB,doaCosC,PI;
   cout<<"Enter the first side: ";
   cin>>a;
   cout<<"Enter the second side: ";
@@ -10,15 +10,15 @@ int main(){
   cout<<"Enter the third side: ";
   cin>>c;
 
-  PI = acos(1.);
+  PI = acos(-1.);
 
-  cosA = ((pow(b,2) + pow(c,2)) - pow(a,2))/(2*b*c); //those brackets surrounding 2*b*c are very important
-  cosB = ((pow(c,2) + pow(a,2)) - pow(b,2))/(2*c*a); //was stucked for about an hour just because of brackets
-  cosC = ((pow(a,2) + pow(b,2)) - pow(c,2))/(2*a*b); //if we don't give bracktes then the pow expression is divided by 2 only
+  doaCosA = ((pow(b,2) + pow(c,2)) - pow(a,2))/(2*b*c); //those brackets surrounding 2*b*c are very important
+  doaCosB = ((pow(c,2) + pow(a,2)) - pow(b,2))/(2*c*a); //was stucked for about an hour just because of brackets
+  doaCosC = ((pow(a,2) + pow(b,2)) - pow(c,2))/(2*a*b); //if we don't give bracktes then the pow expression is divided by 2 only
 
-  A = acos(cosA)*180/PI ; //acos() only works between -1<=x<=1
-  B = acos(cosB)*180/PI; //if brackest are not given perfectly the value can be very large
-  C = acos(cosC)*180/PI; //have multiplied with 180 and divided by PI to convert the radian value to degree
+  A = acos(doaCosA)*180/PI ; //acos() only works between -1<=x<=1
+  B = acos(doaCosB)*180/PI; //if brackest are not given perfectly the value can be very large
+  C = acos(doaCosC)*180/PI; //have multiplied with 180 and divided by PI to convert the radian value to degree
 
   cout<<"The first angle is "<<A<<" degree"<<endl;
   cout<<"The second angle is "<<B<<" degree"<<endl;
